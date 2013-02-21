492bffe5d8310a47462fd4e860f3e736303dce2c
Pavel Krajcevski <pavel@cs.unc.edu>
2013-02-21 15:30:12 -0500

    Add initial game source file.

---
+++ src/main.py
@@ -0,0 +1,25 @@
+################################################################################
+################################################################################
+#
+# main.py
+#
+# This file contains the game loop for the game. It serves as a controller for
+# hooking all of the pieces (rendering, input, updates) together
+#
+################################################################################
+
+import sys
+
+import pygame
+pygame.init()
+
+WINDOW_SIZE_X=800
+WINDOW_SIZE_Y=600
+pygame.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
+
+while True:
+	pygame.display.update()
+	for event in pygame.event.get():
+		if event.type in (pygame.QUIT, pygame.KEYDOWN):
+			sys.exit()
+	
