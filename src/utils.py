40999a04c43788fa8e745a7fae36e8e4d5c9941f
Pavel Krajcevski <pavel@cs.unc.edu>
2013-02-24 10:29:12 -0500

    Add utils.py for random math stuff that doesn't go anywhere else

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+################################################################################
+################################################################################
+#
+# utils.py
+#
+# This file contains all of the utility functions associated with the game. These
+# methods are used all throughout the source but don't really belong to any 
+# specific module...
+#
+################################################################################
+
+import math
+from lib.euclid import *
+
+def lerp(a, b, t):
+	return (1.0 - t)*a + t*b
+
+def polar2cart(angle, dist):
+
+	angle = (90 - angle) * math.pi / 180
+	x = dist * cos(angle)
+	y = dist * sin(angle)
+
+	return Vector2(x, -y)
