1cbcfb515b01f7d3e986145a59b8533fe376aba7
David <bencuya@live.unc.edu>
2013-03-26 15:19:04 -0400

    'message'

--- src/player.py
+++ src/player.py
@@ -9,6 +9,7 @@
 ################################################################################
 
 import pygame, os, time
+import math, re
 import animation
 from eventmanager import Events, InputManager
 from animatedobject import *
@@ -18,12 +19,29 @@ class Player(AnimatedObject):
 
     def __init__(self):
 	
-	self.currentColor = pygame.color.Color("white")
+	self.currentColor = pygame.color.Color("black")
         super(Player, self).__init__("smooth-idle")
         self.startAnimation("smooth-idle", time.clock())
 
         # Initially start in the middle of the screen.
         self.pos = screen2worldPos(Vector2(0, 0), 0.5 * Vector2(screenSizeX(), screenSizeY()))
+
+
+    def changeColor(self,toChangeColor):
+	myAnimations = self.animations
+	idleAnimation = myAnimations["smooth-idle"]
+	myLimb = idleAnimation.limb
+	self.iterateChangeColor(myLimb, toChangeColor)
+	
+	
+    def iterateChangeColor(self, limb, toChangeColor):
+	animationKeys = limb.attribs['colour'].animationKeys
+	for keyframe in animationKeys.keys():
+		animationKeys[keyframe] = Vector3(toChangeColor.r,toChangeColor.g,toChangeColor.b)
+	if limb.children:
+		for child in limb.children:
+			self.iterateChangeColor(child, toChangeColor)
+
 
     def update(self, inputManager):
         if inputManager.isCurrentEvent(Events.MOVE_LEFT):
@@ -33,15 +51,23 @@ class Player(AnimatedObject):
             #move right
             pass
 	elif inputManager.isCurrentEvent(Events.CHANGE_COLOR_1):
-		self.currentColor = pygame.color.Color("red")
-		print(self.currentColor)
+		if (self.currentColor.r == 0):
+			self.currentColor = pygame.color.Color(128, self.currentColor.g, self.currentColor.b)
+		elif (self.currentColor.r == 128):
+			self.currentColor = pygame.color.Color(255, self.currentColor.g, self.currentColor.b)
+		self.changeColor(self.currentColor)
 	elif inputManager.isCurrentEvent(Events.CHANGE_COLOR_2):
-		self.currentColor = pygame.color.Color("green")
-		print(self.currentColor)
+		if (self.currentColor.g == 0):
+			self.currentColor = pygame.color.Color(self.currentColor.r, 128, self.currentColor.b)
+		elif (self.currentColor.g == 128):
+			self.currentColor = pygame.color.Color(self.currentColor.r, 255, self.currentColor.b)
+		self.changeColor(self.currentColor)
 	elif inputManager.isCurrentEvent(Events.CHANGE_COLOR_3):
-		self.currentColor = pygame.color.Color("blue")
-		print(self.currentColor)
+		if (self.currentColor.b == 0):
+			self.currentColor = pygame.color.Color(self.currentColor.r, self.currentColor.g, 128)
+		elif (self.currentColor.b == 128):
+			self.currentColor = pygame.color.Color(self.currentColor.r, self.currentColor.g, 255)
+		self.changeColor(self.currentColor)
 	elif inputManager.isCurrentEvent(Events.RESET_COLOR):
-		self.currentColor = pygame.color.Color("white")
-		print(self.currentColor)
-		
+		self.currentColor = pygame.color.Color("black")
+		self.changeColor(self.currentColor)
