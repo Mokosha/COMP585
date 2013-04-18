bc532b360e0e31b792c2af10b59c777d624767c0
Pavel Krajcevski <pavel@cs.unc.edu>
2013-04-18 18:54:55 -0400

    First pass at animation blending

--- src/animatedobject.py
+++ src/animatedobject.py
@@ -8,12 +8,13 @@
 # 
 ################################################################################
 
-import pygame, os, time
+import pygame, os, time, copy
 import animation
 from gameobject import *
 from utils import *
 
 FPS = 25
+BLENDTIME = 0.2 # The amount of time it takes to blend between two animations in seconds
 
 class AnimatedObject(GameObject):
 
@@ -22,6 +23,9 @@ class AnimatedObject(GameObject):
         self.animations = {}
         self.loadAnim(idle_name, loop)
         self.currentAnim = self.animations[idle_name]
+        self.previousAnim = None
+        self.previousAnimStartTime = None
+        self.currentPose = copy.deepcopy(self.currentAnim.limb)
         self.animStopTime = -1
         self.animStartTime = time.time()
         self.animElapsedTime = 0.0
@@ -41,9 +45,11 @@ class AnimatedObject(GameObject):
         if name == self.currentAnim.name:
             return
 
+        self.previousAnimStartTime = self.animStartTime
         self.animStartTime = time
         self.animElapsedTime = 0.0
         self.animStopTime = time - 1
+        self.previousAnim = self.currentAnim
         self.currentAnim = self.animations[name]
 
     def stopAnimation(self):
@@ -60,6 +66,52 @@ class AnimatedObject(GameObject):
             frame = (frame % (self.currentAnim.getMaxFrame() - 1)) + 1
 
         return frame
+
+    def updatePose(self):
+
+        def updateAttr(attr, newattr, oldattr, t):
+
+            frame = self.getCurrentFrame()
+            oldframe = 0
+            if self.previousAnim:
+                st = self.previousAnimStartTime
+                elapsed = self.animElapsedTime + (self.animStartTime - st)
+                oldframe = FPS * elapsed
+
+                if self.previousAnim.loop:
+                    oldframe = (oldframe % (self.previousAnim.getMaxFrame() - 1)) + 1
+
+            attr.animationKeys = {}
+            if oldattr != None:
+                oldval = oldattr.getValue(oldframe)
+                newval = newattr.getValue(frame)
+                attr.initialValue = smoothstep(oldval, newval, t)
+            else:
+                attr.initialValue = newattr.getValue(frame)
+
+        def updateLimb(limb, newlimb, oldlimb, t):
+
+            for attr in limb.attribs.keys():
+                if oldlimb != None:
+                    updateAttr(limb.attribs[attr], newlimb.attribs[attr], oldlimb.attribs[attr], t)
+                else:
+                    updateAttr(limb.attribs[attr], newlimb.attribs[attr], None, 0)
+
+            assert len(limb.children) == len(newlimb.children)
+            assert oldlimb == None or len(limb.children) == len(oldlimb.children)
+
+            for idx in range(len(limb.children)):
+                if oldlimb == None:
+                    updateLimb(limb.children[idx], newlimb.children[idx], None, t)
+                else:
+                    updateLimb(limb.children[idx], newlimb.children[idx], oldlimb.children[idx], t)
+
+        newlimb = self.currentAnim.limb
+        oldlimb = None
+        if self.previousAnim != None:
+            oldlimb = self.previousAnim.limb
+
+        updateLimb(self.currentPose, newlimb, oldlimb, self.animElapsedTime / BLENDTIME)
 
     def resetAABB(self):
 
@@ -92,9 +144,14 @@ class AnimatedObject(GameObject):
         if self.animStopTime > self.animStartTime:
             self.animElapsedTime = self.animStopTime - self.animStartTime
 
+        if self.animElapsedTime > BLENDTIME:
+            self.previousAnim = None
+            self.previousAnimStartTime = None
+
+        self.updatePose()
         self.resetAABB()
 
     def render(self, surface, campos):
         super(AnimatedObject, self).render(surface, campos)
         renderpos = world2screenPos(campos, self.pos)
-        self.currentAnim.draw(self.getCurrentFrame(), surface, renderpos, self.color)
+        self.currentPose.draw(self.getCurrentFrame(), surface, renderpos, 0, self.color)
